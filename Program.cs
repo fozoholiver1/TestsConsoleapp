535030c04570b5efefbe32f95baa9c4650bee333
oliver <dopplereffect1992@gmail.com>
2020-01-31 21:04:11 -0800

    this program takes a name and age and print them together but cannot print a string togeter with an integer, this problem will be solved in the next commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,16 @@ namespace TestsConsoleapp
     {
         static void Main(string[] args)
         {
-        }
+            Console.WriteLine(" enter your name");//this program takes a name and age and print them together
+            string name = Console.ReadLine();
+            Console.WriteLine(" enter your age");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("hello " + name + " you are " + age 
+                
+                + " years old");
+
+            Console.ReadLine();
+        }   
     }
 }
