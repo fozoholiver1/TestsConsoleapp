e1b263621ffe229efa10febec8bad667e2154702
oliver <dopplereffect1992@gmail.com>
2020-02-05 03:08:52 -0800

    acessing the private method with the geter and seter metthod

---
+++ Movie.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsConsoleapp
+{
+    class Movie
+    {
+        public string title;
+        public string director;
+        private string rating;
+
+        public Movie(string atitle, string adirector, string arating) //our construct
+        {
+            title = atitle;
+            director = adirector;
+            Rating = arating;
+        }
+            //since rating is a private variable we have to create a get and setter for the ratings
+        public string Rating
+        {
+            get{ //this will get any value passed to the ratting variable 
+                return rating;
+            }
+            set//this will compare the ratings with these set value to either proceed or to return a default value
+            {
+                if (rating == "PG"||value=="PG-13"||value=="R"||value=="NR"||value=="G")
+                {
+
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            }
+            
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,15 +10,11 @@ namespace TestsConsoleapp
     {
         static void Main(string[] args)
         {
-            Student student1 = new Student("john" ,"arts",3.6) ;//the constructor allows us to pass values as variables in our program 
+            Movie movie1 = new Movie("war room" ,"oliver","PjjG") ;//the constructor allows us to pass values as variables in our program 
 
-            Student student2 = new Student("oliver", "chemistry", 2.6);//the constructor allows us to pass values as variables in our program  
+            Movie movie2 = new Movie("oliver", "chemistry", "PG-13");//the constructor allows us to pass values as variables in our program  
 
-            Console.WriteLine(student1.name);//this will print ou the title of our first book
-
-            // gettiing results from our construct methods 
-            Console.WriteLine(student1.Hashornors());//checking wether student on has hornors or not from our class sam in the precieding line
-            Console.WriteLine(student2.Hashornors());
+            Console.WriteLine(movie1.Rating);
             Console.ReadLine(); 
         }
       
