a069b07052d8e0cf149fe92bea20c787eb94e323
oliver <dopplereffect1992@gmail.com>
2020-02-04 21:57:14 -0800

    using the try and catch blocks we can display an error message each time the code breaks

--- Program.cs
+++ Program.cs
@@ -10,17 +10,27 @@ namespace TestsConsoleapp
     {
         static void Main(string[] args)
         {
+            try//we put all the code that can fail inside of a try and catch block,such that if the program fails we can tell what went wrong
+            {
 
-            Console.WriteLine("a program to divide two numbers");
+                Console.WriteLine("a program to divide two numbers");
 
-            Console.WriteLine("enter the first number");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter the first number");
+                int num1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("enter the second number");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter the second number");
+                int num2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine(num1 / num2);//this will divide the two numbers,remember divition by zero is forbidden
+                Console.WriteLine(num1 / num2);//this will divide the two numbers,remember divition by zero is forbidden
 
+            }
+            catch
+            {
+
+                Console.WriteLine("error");
+            }
+            Console.ReadLine();
+           
         }
       
     }
