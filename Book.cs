f9dbb753e36bc346a14d99d919bec84b4f2f9a53
oliver <dopplereffect1992@gmail.com>
2020-02-05 01:12:23 -0800

    constructors in classes

---
+++ Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsConsoleapp
+{
+    class Book
+    {
+        public string author;
+        public string title;
+        public int pages;
+        //this is the beginning of ou constructor
+        public Book(string iauthor , string ititle, int ipages)//this is our constructor ,these variables are different from the ones in our class
+        {
+            author = iauthor;//assigning variable to class variables 
+            title = ititle;
+            pages = ipages;
+
+        }
+    }
+    
+}
--- Program.cs
+++ Program.cs
@@ -10,34 +10,12 @@ namespace TestsConsoleapp
     {
         static void Main(string[] args)
         {
-            try//we put all the code that can fail inside of a try and catch block,such that if the program fails we can tell what went wrong
-            {
+            Book book1 = new Book(" the mine of life" ,"oliver",7978798) ;//the constructor allows us to pass values as variables in our program  
+            Book book2 = new Book(" the help of life", "mike doe", 09987);//this the second instance of oure book class    
+            //prop[iiopfoifoifoifyfuyfiyfiuu]
+            Console.WriteLine(book1.title);//this will print ou the title of our first book
 
-
-                Console.WriteLine("a program to divide two numbers");
-
-                Console.WriteLine("enter the first number");
-                int num1 = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("enter the second number");
-                int num2 = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine(num1 / num2);//this will divide the two numbers,remember divition by zero is forbidden
-
-            }
-            //we can specify our error messages such that in case of a breakage the program continues
-            catch(DivideByZeroException e )//this will get and display the exact error mesage with details of what went wrong
-            {
-
-                Console.WriteLine(e.Message);//this is our error message
-            }
-            catch (FormatException e)//this will get and display the exact error mesage with details of what went wrong
-            {
-
-                Console.WriteLine(e.Message);//this is our error message
-            }
-            Console.ReadLine();
-           
+            Console.ReadLine(); 
         }
       
     }
